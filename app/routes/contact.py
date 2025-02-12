4e7c4a0249e3e576100368994157d7aec42b6c08
Jason Wu <wu80jason8@gmail.com>
2025-02-11 22:50:02 -0800

    type additions

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,12 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Body
 from ..models.form import Form
+from typing import Annotated
 
 router = APIRouter(prefix="/contact")
 
 
-@router.get("/send-form", tags=["contact"])
-def sendContactEmail(form: Form):
+@router.get("/send-form", tags=["contact"], status_code=status.HTTP_201_CREATED)
+async def sendContactEmail(form: Annotated[Form, Body(title="the form a user submitted to contact me")]):
+    
     """
     Add the contents of the form into a database to store it, and then forward an email with the contents of the form to your own email
     """
+    
+    try:
+        return {
+            "message": "Successfully sent contact form!"
+        }
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unable to process form request."
+        )
 
