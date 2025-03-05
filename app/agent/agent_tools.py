116d28f51d3937985b55b46a8cf9c42c9080ce52
Jason Wu <wu80jason8@gmail.com>
2025-03-04 20:02:20 -0800

    navigation tool done

--- app/agent/agent_tools.py
+++ app/agent/agent_tools.py
@@ -2,8 +2,17 @@ from langchain_core.tools import tool
 from pydantic import HttpUrl, ValidationError, BaseModel
 from typing import Annotated
 from typing import Optional
-from qdrant_client import AsyncQdrantClient
+import asyncio
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+from langchain_community.embeddings import JinaEmbeddings
+from langchain_core.documents import Document
+from groq import AsyncGroq
+import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 """Create agent with langchain tools. Implement it asyncronously with running background tasks as well to more quickly parse files into vector db"""
 
@@ -19,13 +28,16 @@ class Link(BaseModel):
     link: HttpUrl
     
 
+groq_client = AsyncGroq(api_key=os.getenv("GROQ_API_KEY"))
+    
+
 # convert this to json and pass it into the prompt template of the llm to let it decide
 # button name will be the name property here
 # essentially will be a dynamic link based upon what the user inputs
 LINK_INFO: list[Link] = [
     {
         "name": "About Page",
-        "description": "Gives detailed information about myself with also my resume on there",
+        "description": "This essentially provides an overview of my personal information (i.e. where I go for university, what I study, what I like to do, etc.)",
         "link": "http://localhost:3000/about"
     },
     {
@@ -55,19 +67,52 @@ PROJECT_INFORMATION = {
         "tech_stack": ["FastAPI", "LangChain", "OpenAI API", "Next.js", "Google Cloud", "Python", "TypeScript"]
     }
 }
-@tool(response_format='content_and_artifact')
-async def navigation_tool() -> NavigationToolResponse:
+@tool(response_format='content')
+async def navigation_tool(query: str) -> str:
     """
     based upon the user's query, should be able to figure out the route to a webpage to get more information (or even linkedin or github)
     """
+    qdrant_client = QdrantClient(url=os.getenv("QDRANT_URL"), api_key=os.getenv("QDRANT_API_KEY"))
+    vector_store = QdrantVectorStore(client=qdrant_client, embedding=JinaEmbeddings(), collection_name="links")
+    retriever = vector_store.as_retriever(search_type="similarity_score_threshold", search_kwargs={
+        "k": 3,
+        "score_threshold": 0.7
+    })
+    response: list[Document] = await retriever.ainvoke(query)
+    for i in range(len(response)):
+        response[i] = response[i].model_dump_json(include={"metadata", "page_content"}, indent=2)
+    response = json.dumps(response)
+    groq_res = await groq_client.chat.completions.create(messages=[
+        {
+            "role": "system",
+            "content": f"You will be given an JSON string of a couple links with their descriptions. Based upon the user's query, provide the link in JSON format which best answers the user's question. If none of the links provided suffice the query, provide an empty url link.\nHere are the links: {response}\n\n"
+            f"The JSON object must use the schema: {json.dumps(NavigationToolResponse.model_json_schema(), indent=2)} "
+        },
+        {
+            "role": "user",
+            "content": query
+        }
+    ], model="gemma2-9b-it", temperature=0.3, top_p=1, stream=False, response_format={
+        "type": "json_object"
+    })
+    
+    final_url = json.loads(groq_res.choices[0].to_dict()["message"]["content"])["url"]
+    return final_url
     
 
 @tool(response_format="content_and_artifact")
 def obtainProjectInformation(projectName: str) -> ProjectInformationToolResponse:
     """
-    Pass in the project name and from the project name obtain extra details like the tech stack about the project. This tool is only used if the retrieval from vector database cannot yield any firm data on a specific project
+    Pass in the project name and from the project name obtain extra details like the tech stack about the project. This tool is only used if the retrieval from vector database cannot yield any firm data on a specific project.
     """
     projectDetails = PROJECT_INFORMATION.get(projectName, "No details found on project.")
     return ProjectInformationToolResponse(
         **projectDetails
     ).model_dump_json(indent=2)
+
+async def testing():
+    response = await navigation_tool.ainvoke("What projects has Jason worked on?")
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(testing())
